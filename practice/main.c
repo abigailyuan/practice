f2ccae09841ec25977021b3a098789144d629431
abigailyuan <apple@appledeMacBook-Pro.local>
2014-10-14 08:29:45 +0800

    Initial Commit

---
+++ practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  practice
+//
+//  Created by abigailyuan on 10/14/14.
+//  Copyright (c) 2014 abigailyuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
